29d690f425bc0ae916f1698dc6d03e181d167bdf
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:16:57 -0500

    Infrastructure/ Reposoties  - PacienteRepository

--- Clinica.Infrastructure/Repositories/PacienteRepository.cs
+++ Clinica.Infrastructure/Repositories/PacienteRepository.cs
@@ -7,5 +7,22 @@ namespace Clinica.Infrastructure.Repositories;
 
 public class PacienteRepository : GenericRepository<Paciente>, IPacienteRepository
 {
-    
+    public PacienteRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Paciente?> ObtenerPorDniAsync(string dni)
+    {
+        return await Context.Pacientes
+            .Include(x => x.HistorialClinico)
+            .FirstOrDefaultAsync(x => x.DNI == dni);
+    }
+
+    public async Task<Paciente?> ObtenerConHistorialAsync(Guid pacienteId)
+    {
+        return await Context.Pacientes
+            .Include(x => x.HistorialClinico)
+            .ThenInclude(x => x!.Detalles)
+            .FirstOrDefaultAsync(x => x.Id == pacienteId);
+    }
 }
