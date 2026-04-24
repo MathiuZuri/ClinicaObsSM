6d200a7aa9bb88e1aa62011fd6fb341fb959fd21
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:20:28 -0500

    Infrastructure/ Reposoties  - PagoRepository

--- Clinica.Infrastructure/Repositories/PagoRepository.cs
+++ Clinica.Infrastructure/Repositories/PagoRepository.cs
@@ -1,6 +1,45 @@
-﻿namespace Clinica.Infrastructure.Repositories;
+﻿using Clinica.Domain.Entities;
+using Clinica.Domain.Interfaces;
+using Clinica.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
-public class PagoRepository
+namespace Clinica.Infrastructure.Repositories;
+
+public class PagoRepository : GenericRepository<Pago>, IPagoRepository
 {
-    
+    public PagoRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Pago>> ObtenerPorPacienteAsync(Guid pacienteId)
+    {
+        return await Context.Pagos
+            .Include(x => x.Paciente)
+            .Include(x => x.ServicioClinico)
+            .Include(x => x.Cita)
+            .Include(x => x.Atencion)
+            .Where(x => x.PacienteId == pacienteId)
+            .OrderByDescending(x => x.FechaPago)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Pago>> ObtenerPorCitaAsync(Guid citaId)
+    {
+        return await Context.Pagos
+            .Include(x => x.Paciente)
+            .Include(x => x.ServicioClinico)
+            .Where(x => x.CitaId == citaId)
+            .OrderByDescending(x => x.FechaPago)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Pago>> ObtenerPorAtencionAsync(Guid atencionId)
+    {
+        return await Context.Pagos
+            .Include(x => x.Paciente)
+            .Include(x => x.ServicioClinico)
+            .Where(x => x.AtencionId == atencionId)
+            .OrderByDescending(x => x.FechaPago)
+            .ToListAsync();
+    }
 }
