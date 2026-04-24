dddf4933c3c8c45d5f3f3b2107c23dda5274d64e
kevinraf <paricahuak40@gmail.com>
2026-04-24 10:51:13 -0500

    DOMAIN/ Enums - TipoAccionAuditoria

--- Clinica.Domain/Enums/TipoAccionAuditoria.cs
+++ Clinica.Domain/Enums/TipoAccionAuditoria.cs
@@ -1,6 +1,33 @@
 ﻿namespace Clinica.Domain.Enums;
 
-public class TipoAccionAuditoria
+public enum TipoAccionAuditoria
 {
-    
+    InicioSesion = 1,
+    CierreSesion = 2,
+    InicioSesionFallido = 3,
+
+    Crear = 10,
+    Editar = 11,
+    Eliminar = 12,
+    EliminarLogico = 13,
+    CambiarEstado = 14,
+    Consultar = 15,
+
+    ProgramarCita = 20,
+    ReprogramarCita = 21,
+    CancelarCita = 22,
+    RegistrarAtencion = 23,
+    CerrarAtencion = 24,
+
+    RegistrarPago = 30,
+    AnularPago = 31,
+    ReembolsarPago = 32,
+
+    AsignarRol = 40,
+    QuitarRol = 41,
+    AsignarPermiso = 42,
+    QuitarPermiso = 43,
+
+    ExportarReporte = 50,
+    ErrorSistema = 99
 }
