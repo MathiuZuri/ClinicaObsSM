b2a89a17caafeac6887ed0518afa27b938be2a55
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:41:25 -0500

    DOMAIN/ DTOS - RegistrarAtencionDto

--- Clinica.Domain/DTOs/Atenciones/RegistrarAtencionDto.cs
+++ Clinica.Domain/DTOs/Atenciones/RegistrarAtencionDto.cs
@@ -2,5 +2,19 @@
 
 public class RegistrarAtencionDto
 {
-    
+    public Guid PacienteId { get; set; }
+    public Guid DoctorId { get; set; }
+    public Guid ServicioClinicoId { get; set; }
+    public Guid? CitaId { get; set; }
+    public Guid HistorialClinicoId { get; set; }
+
+    public string MotivoConsulta { get; set; } = string.Empty;
+    public string? Observaciones { get; set; }
+    public string? DiagnosticoResumen { get; set; }
+    public string? Indicaciones { get; set; }
+    public string? Tratamiento { get; set; }
+
+    public decimal CostoFinal { get; set; }
+
+    public Guid? UsuarioRegistroId { get; set; }
 }
