bcb55d554e2e5393bd7900ef4d6c030d098fe106
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:29:30 -0500

    API/  Services -  IPermisoService

--- Clinica.API/Services/IPermisoService.cs
+++ Clinica.API/Services/IPermisoService.cs
@@ -1,6 +1,8 @@
-﻿namespace Clinica.API.Services;
+﻿using Clinica.Domain.DTOs.Permisos;
+
+namespace Clinica.API.Services;
 
 public interface IPermisoService
 {
-    
+    Task<IEnumerable<PermisoResponseDto>> ObtenerTodosAsync();
 }
