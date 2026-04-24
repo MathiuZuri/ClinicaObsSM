f46189ea46c87c23b003f236e3e10aba8acbe515
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:33:28 -0500

    DOMAIN/ DTOS - EditarDoctorDto

--- Clinica.Domain/DTOs/Doctores/EditarDoctorDto.cs
+++ Clinica.Domain/DTOs/Doctores/EditarDoctorDto.cs
@@ -1,6 +1,18 @@
-﻿namespace Clinica.Domain.DTOs.Doctores;
+﻿using Clinica.Domain.Enums;
+
+namespace Clinica.Domain.DTOs.Doctores;
 
 public class EditarDoctorDto
 {
-    
+    public string CMP { get; set; } = string.Empty;
+    public string Nombres { get; set; } = string.Empty;
+    public string Apellidos { get; set; } = string.Empty;
+    public string Especialidad { get; set; } = string.Empty;
+    public string? Celular { get; set; }
+    public string? Correo { get; set; }
+
+    public DateTime FechaInicioContrato { get; set; }
+    public DateTime? FechaFinContrato { get; set; }
+
+    public EstadoDoctor Estado { get; set; }
 }
