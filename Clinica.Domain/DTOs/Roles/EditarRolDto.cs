0d0593074cc146d20dd0b566b145cd2d9da5b0d2
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:25:57 -0500

    DOMAIN/ DTOS - EditarRolDto

--- Clinica.Domain/DTOs/Roles/EditarRolDto.cs
+++ Clinica.Domain/DTOs/Roles/EditarRolDto.cs
@@ -2,5 +2,7 @@
 
 public class EditarRolDto
 {
-    
+    public string Nombre { get; set; } = string.Empty;
+    public string? Descripcion { get; set; }
+    public bool Activo { get; set; } = true;
 }
