8fb66fc11e6bc5f555c553de366d7a50cb1546db
kevinraf <paricahuak40@gmail.com>
2026-04-24 10:48:43 -0500

    DOMAIN/ Enums - EstadoPaciente

--- Clinica.Domain/Enums/EstadoPaciente.cs
+++ Clinica.Domain/Enums/EstadoPaciente.cs
@@ -1,6 +1,10 @@
 ﻿namespace Clinica.Domain.Enums;
 
-public class EstadoPaciente
+public enum EstadoPaciente
 {
-    
+    Activo = 1,
+    Inactivo = 2,
+    Fallecido = 3,
+    Bloqueado = 4,
+    Eliminado = 5
 }
