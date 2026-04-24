b17772d8d69704de0eb4828ad20496b05a9df117
kevinraf <paricahuak40@gmail.com>
2026-04-24 10:56:16 -0500

    DOMAIN/ Interfaces - IPacienteRepository

--- Clinica.Domain/Interfaces/IPacienteRepository.cs
+++ Clinica.Domain/Interfaces/IPacienteRepository.cs
@@ -4,14 +4,6 @@ namespace Clinica.Domain.Interfaces;
 
 public interface IPacienteRepository : IGenericRepository<Paciente>
 {
-    // Buscamos al paciente por su DNI
     Task<Paciente?> ObtenerPorDniAsync(string dni);
-
-    // Buscamos por Número de Historia Clínica
-    Task<Paciente?> ObtenerPorNumeroHcAsync(string numeroHc);
-
-    // Validaciones rápidas para evitar duplicados antes de guardar
-    Task<bool> ExisteDniAsync(string dni);
-    Task<bool> ExisteNumeroHcAsync(string numeroHc);
-    Task<int> ContarPacientesPorAnioAsync(int anio);
+    Task<Paciente?> ObtenerConHistorialAsync(Guid pacienteId);
 }
