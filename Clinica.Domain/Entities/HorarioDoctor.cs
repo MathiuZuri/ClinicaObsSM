372e9627832320b61c2e5e77e1fad96fee0caa84
kevinraf <paricahuak40@gmail.com>
2026-04-24 10:42:13 -0500

    DOMAIN/Entities - HorarioDoctor

--- Clinica.Domain/Entities/HorarioDoctor.cs
+++ Clinica.Domain/Entities/HorarioDoctor.cs
@@ -2,5 +2,20 @@
 
 public class HorarioDoctor
 {
-    
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public Guid DoctorId { get; set; }
+    public Doctor Doctor { get; set; } = null!;
+
+    public DayOfWeek DiaSemana { get; set; }
+
+    public TimeOnly HoraInicio { get; set; }
+    public TimeOnly HoraFin { get; set; }
+
+    public DateOnly FechaInicioVigencia { get; set; }
+    public DateOnly? FechaFinVigencia { get; set; }
+
+    public bool Activo { get; set; } = true;
+
+    public ICollection<Cita> Citas { get; set; } = new List<Cita>();
 }
