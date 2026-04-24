ed474d99f9aa98e6b0b20bda2b01eb3ed71cf8df
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:26:36 -0500

    DOMAIN/ DTOS - PermisoResponseDto

--- Clinica.Domain/DTOs/Permisos/PermisoResponseDto.cs
+++ Clinica.Domain/DTOs/Permisos/PermisoResponseDto.cs
@@ -2,5 +2,10 @@
 
 public class PermisoResponseDto
 {
-    
+    public Guid Id { get; set; }
+    public string Codigo { get; set; } = string.Empty;
+    public string Nombre { get; set; } = string.Empty;
+    public string Modulo { get; set; } = string.Empty;
+    public string? Descripcion { get; set; }
+    public bool Activo { get; set; }
 }
