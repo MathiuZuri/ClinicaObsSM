b552aa6b60c9c14841baf188a9c9ba8e16f9ecea
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:14:40 -0500

    Infrastructure/ Reposoties  - UsuarioRepository

--- Clinica.Infrastructure/Repositories/UsuarioRepository.cs
+++ Clinica.Infrastructure/Repositories/UsuarioRepository.cs
@@ -1,6 +1,29 @@
-﻿namespace Clinica.Infrastructure.Repositories;
+﻿using Clinica.Domain.Entities;
+using Clinica.Domain.Interfaces;
+using Clinica.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
-public class UsuarioRepository
+namespace Clinica.Infrastructure.Repositories;
+
+public class UsuarioRepository : GenericRepository<Usuario>, IUsuarioRepository
 {
-    
+    public UsuarioRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Usuario?> ObtenerPorCorreoAsync(string correo)
+    {
+        return await Context.Usuarios
+            .Include(x => x.UsuarioRoles)
+            .ThenInclude(x => x.Rol)
+            .FirstOrDefaultAsync(x => x.Correo == correo);
+    }
+
+    public async Task<Usuario?> ObtenerPorUserNameAsync(string userName)
+    {
+        return await Context.Usuarios
+            .Include(x => x.UsuarioRoles)
+            .ThenInclude(x => x.Rol)
+            .FirstOrDefaultAsync(x => x.UserName == userName);
+    }
 }
