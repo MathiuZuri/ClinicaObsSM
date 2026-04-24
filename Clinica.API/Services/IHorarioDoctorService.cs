42dc699ce818a402c574ae5a86b47d065bdccfe0
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:30:18 -0500

    API/  Services -  IHorarioDoctorService

--- Clinica.API/Services/IHorarioDoctorService.cs
+++ Clinica.API/Services/IHorarioDoctorService.cs
@@ -1,6 +1,11 @@
-﻿namespace Clinica.API.Services;
+﻿using Clinica.Domain.DTOs.Horarios;
+
+namespace Clinica.API.Services;
 
 public interface IHorarioDoctorService
 {
-    
+    Task<IEnumerable<HorarioDoctorResponseDto>> ObtenerTodosAsync();
+    Task<IEnumerable<HorarioDoctorResponseDto>> ObtenerPorDoctorAsync(Guid doctorId);
+    Task<Guid> CrearAsync(CrearHorarioDoctorDto dto);
+    Task ActualizarAsync(Guid id, EditarHorarioDoctorDto dto);
 }
