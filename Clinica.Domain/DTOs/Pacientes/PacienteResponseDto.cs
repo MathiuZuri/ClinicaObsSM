bdfec175bf82b16450769ddf8ba00261d6ae68ed
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:33:03 -0500

    DOMAIN/ DTOS - PacienteResponseDto

--- Clinica.Domain/DTOs/Pacientes/PacienteResponseDto.cs
+++ Clinica.Domain/DTOs/Pacientes/PacienteResponseDto.cs
@@ -1,10 +1,22 @@
-﻿namespace Clinica.Domain.DTOs;
+﻿using Clinica.Domain.Enums;
 
-public record PacienteResponseDto(
-    Guid Id, 
-    string DNI, 
-    string NumeroHC, 
-    string Nombres, 
-    string Apellidos, 
-    DateTime FechaNacimiento,
-    string? Celular);
+namespace Clinica.Domain.DTOs.Pacientes;
+
+public class PacienteResponseDto
+{
+    public Guid Id { get; set; }
+    public string CodigoPaciente { get; set; } = string.Empty;
+    public string DNI { get; set; } = string.Empty;
+    public string Nombres { get; set; } = string.Empty;
+    public string Apellidos { get; set; } = string.Empty;
+    public string NombreCompleto => $"{Nombres} {Apellidos}";
+    public DateTime FechaNacimiento { get; set; }
+    public string Sexo { get; set; } = string.Empty;
+    public string? Celular { get; set; }
+    public string? Correo { get; set; }
+    public string? Direccion { get; set; }
+    public EstadoPaciente Estado { get; set; }
+    public DateTime FechaRegistro { get; set; }
+
+    public string? CodigoHistorial { get; set; }
+}
