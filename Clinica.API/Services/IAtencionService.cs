033d2fae99bdda1489a9c60071a3564c49f0d28c
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:31:07 -0500

    API/  Services -  IAtencionService

--- Clinica.API/Services/IAtencionService.cs
+++ Clinica.API/Services/IAtencionService.cs
@@ -1,6 +1,11 @@
-﻿namespace Clinica.API.Services;
+﻿using Clinica.Domain.DTOs.Atenciones;
+
+namespace Clinica.API.Services;
 
 public interface IAtencionService
 {
-    
+    Task<IEnumerable<AtencionResponseDto>> ObtenerPorPacienteAsync(Guid pacienteId);
+    Task<AtencionResponseDto?> ObtenerPorCitaAsync(Guid citaId);
+    Task<Guid> RegistrarAsync(RegistrarAtencionDto dto);
+    Task CerrarAsync(Guid id, CerrarAtencionDto dto);
 }
