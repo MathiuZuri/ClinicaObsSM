0b5edc866efef78da8751e075c8cf8ed81e6ae14
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:33:58 -0500

    DOMAIN/ DTOS - CrearHorarioDoctorDto

--- Clinica.Domain/DTOs/Horarios/CrearHorarioDoctorDto.cs
+++ Clinica.Domain/DTOs/Horarios/CrearHorarioDoctorDto.cs
@@ -2,5 +2,10 @@
 
 public class CrearHorarioDoctorDto
 {
-    
+    public Guid DoctorId { get; set; }
+    public DayOfWeek DiaSemana { get; set; }
+    public TimeOnly HoraInicio { get; set; }
+    public TimeOnly HoraFin { get; set; }
+    public DateOnly FechaInicioVigencia { get; set; }
+    public DateOnly? FechaFinVigencia { get; set; }
 }
