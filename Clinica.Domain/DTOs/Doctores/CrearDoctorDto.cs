720bfe473a9249d83ed7c13dcbe8601772f7be1d
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:33:15 -0500

    DOMAIN/ DTOS - CrearDoctorDto

--- Clinica.Domain/DTOs/Doctores/CrearDoctorDto.cs
+++ Clinica.Domain/DTOs/Doctores/CrearDoctorDto.cs
@@ -2,5 +2,15 @@
 
 public class CrearDoctorDto
 {
-    
+    public string CMP { get; set; } = string.Empty;
+    public string Nombres { get; set; } = string.Empty;
+    public string Apellidos { get; set; } = string.Empty;
+    public string Especialidad { get; set; } = string.Empty;
+    public string? Celular { get; set; }
+    public string? Correo { get; set; }
+
+    public DateTime FechaInicioContrato { get; set; }
+    public DateTime? FechaFinContrato { get; set; }
+
+    public Guid UsuarioId { get; set; }
 }
