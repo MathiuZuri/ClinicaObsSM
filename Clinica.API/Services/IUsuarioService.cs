47259a56435a1c906aa4ecff00117fcbc77ebe8e
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:29:04 -0500

    API/  Services -  IUsuarioService

--- Clinica.API/Services/IUsuarioService.cs
+++ Clinica.API/Services/IUsuarioService.cs
@@ -1,6 +1,12 @@
-﻿namespace Clinica.API.Services;
+﻿using Clinica.Domain.DTOs.Usuarios;
+
+namespace Clinica.API.Services;
 
 public interface IUsuarioService
 {
-    
+    Task<IEnumerable<UsuarioResponseDto>> ObtenerTodosAsync();
+    Task<UsuarioResponseDto?> ObtenerPorIdAsync(Guid id);
+    Task<Guid> CrearAsync(CrearUsuarioDto dto);
+    Task ActualizarAsync(Guid id, EditarUsuarioDto dto);
+    Task AsignarRolAsync(AsignarRolUsuarioDto dto);
 }
