4d08ac8a88094a489bc83685204b107611a768ed
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:43:31 -0500

    API/  Services IMP -  AuthController

--- Clinica.API/Controllers/AuthController.cs
+++ Clinica.API/Controllers/AuthController.cs
@@ -1,6 +1,31 @@
-﻿namespace Clinica.API.Controllers;
+﻿using Clinica.API.Services;
+using Clinica.Domain.DTOs.Auth;
+using Microsoft.AspNetCore.Mvc;
 
-public class AuthController
+namespace Clinica.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
 {
-    
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] IniciarSesionDto dto)
+    {
+        try
+        {
+            var respuesta = await _authService.IniciarSesionAsync(dto);
+            return Ok(respuesta);
+        }
+        catch (InvalidOperationException ex)
+        {
+            return Unauthorized(new { mensaje = ex.Message });
+        }
+    }
 }
