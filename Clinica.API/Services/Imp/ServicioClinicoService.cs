e1a44776799fe215920fe1b7dbe2617e4a709aba
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:39:03 -0500

    API/  Services IMP -  ServicioClinicoService

--- Clinica.API/Services/Imp/ServicioClinicoService.cs
+++ Clinica.API/Services/Imp/ServicioClinicoService.cs
@@ -1,6 +1,52 @@
-﻿namespace Clinica.API.Services.Imp;
+﻿using Clinica.Domain.DTOs.Servicios;
+using Clinica.Domain.Interfaces;
 
-public class ServicioClinicoService
+namespace Clinica.API.Services.Imp;
+
+public class ServicioClinicoService : IServicioClinicoService
 {
-    
+    private readonly IServicioClinicoRepository _servicioRepository;
+
+    public ServicioClinicoService(IServicioClinicoRepository servicioRepository)
+    {
+        _servicioRepository = servicioRepository;
+    }
+
+    public async Task<IEnumerable<ServicioClinicoResponseDto>> ObtenerTodosAsync()
+    {
+        var servicios = await _servicioRepository.GetAllAsync();
+
+        return servicios.Select(MapearServicio);
+    }
+
+    public async Task<IEnumerable<ServicioClinicoResponseDto>> ObtenerActivosAsync()
+    {
+        var servicios = await _servicioRepository.ObtenerActivosAsync();
+
+        return servicios.Select(MapearServicio);
+    }
+
+    public async Task<ServicioClinicoResponseDto?> ObtenerPorIdAsync(Guid id)
+    {
+        var servicio = await _servicioRepository.GetByIdAsync(id);
+        if (servicio == null) return null;
+
+        return MapearServicio(servicio);
+    }
+
+    private static ServicioClinicoResponseDto MapearServicio(Clinica.Domain.Entities.ServicioClinico servicio)
+    {
+        return new ServicioClinicoResponseDto
+        {
+            Id = servicio.Id,
+            CodigoServicio = servicio.CodigoServicio,
+            Nombre = servicio.Nombre,
+            Descripcion = servicio.Descripcion,
+            CostoBase = servicio.CostoBase,
+            DuracionMinutos = servicio.DuracionMinutos,
+            RequiereCita = servicio.RequiereCita,
+            GeneraHistorial = servicio.GeneraHistorial,
+            Estado = servicio.Estado
+        };
+    }
 }
