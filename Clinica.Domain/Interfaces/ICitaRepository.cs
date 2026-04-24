7b469ab59c185d38c28d51efe7857eb346dbf425
kevinraf <paricahuak40@gmail.com>
2026-04-24 10:57:00 -0500

    DOMAIN/ Interfaces - ICitaRepository

--- Clinica.Domain/Interfaces/ICitaRepository.cs
+++ Clinica.Domain/Interfaces/ICitaRepository.cs
@@ -4,10 +4,13 @@ namespace Clinica.Domain.Interfaces;
 
 public interface ICitaRepository : IGenericRepository<Cita>
 {
-    // Métodos específicos que la clínica necesita para operar
-    Task<IReadOnlyList<Cita>> ObtenerCitasPorPacienteAsync(Guid pacienteId);
-    Task<IReadOnlyList<Cita>> ObtenerCitasDelDiaPorMedicoAsync(Guid personalMedicoId, DateTime fecha);
-    Task<bool> ExisteInterferenciaDeHorarioAsync(Guid personalMedicoId, DateTime fechaHora, int duracionMinutos);
-}
+    Task<bool> ExisteInterferenciaHorarioAsync(
+        Guid doctorId,
+        DateOnly fecha,
+        TimeOnly horaInicio,
+        TimeOnly horaFin,
+        Guid? citaIdExcluir = null);
 
-//(Deberás crear interfaces similares como IUsuarioRepository para buscar usuarios por correo al iniciar sesión)
+    Task<IEnumerable<Cita>> ObtenerPorPacienteAsync(Guid pacienteId);
+    Task<IEnumerable<Cita>> ObtenerPorDoctorAsync(Guid doctorId);
+}
