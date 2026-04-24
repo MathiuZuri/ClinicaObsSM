2d37909c1fd40124c043ff9f709112b14c6c3c19
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:41:37 -0500

    DOMAIN/ DTOS - CerrarAtencionDto

--- Clinica.Domain/DTOs/Atenciones/CerrarAtencionDto.cs
+++ Clinica.Domain/DTOs/Atenciones/CerrarAtencionDto.cs
@@ -2,5 +2,10 @@
 
 public class CerrarAtencionDto
 {
-    
+    public string? DiagnosticoResumen { get; set; }
+    public string? Indicaciones { get; set; }
+    public string? Tratamiento { get; set; }
+    public string? ObservacionesFinales { get; set; }
+
+    public Guid? UsuarioRegistroId { get; set; }
 }
