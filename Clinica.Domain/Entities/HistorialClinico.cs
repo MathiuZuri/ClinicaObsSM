c84a1b177dd19021f38acebc247f75e250632f74
kevinraf <paricahuak40@gmail.com>
2026-04-24 10:43:59 -0500

    DOMAIN/Entities - HistorialClinico

--- Clinica.Domain/Entities/HistorialClinico.cs
+++ Clinica.Domain/Entities/HistorialClinico.cs
@@ -1,6 +1,19 @@
-﻿namespace Clinica.Domain.Entities;
+﻿using Clinica.Domain.Enums;
+
+namespace Clinica.Domain.Entities;
 
 public class HistorialClinico
 {
-    
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string CodigoHistorial { get; set; } = string.Empty;
+
+    public Guid PacienteId { get; set; }
+    public Paciente Paciente { get; set; } = null!;
+
+    public DateTime FechaApertura { get; set; } = DateTime.UtcNow;
+
+    public EstadoHistorialClinico Estado { get; set; } = EstadoHistorialClinico.Activo;
+
+    public ICollection<HistorialDetalle> Detalles { get; set; } = new List<HistorialDetalle>();
 }
