6ff0022e8589b6e9cdf0b4e5a8444af6ba76f8cf
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:29:40 -0500

    API/  Services -  IAuditoriaService

--- Clinica.API/Services/IAuditoriaService.cs
+++ Clinica.API/Services/IAuditoriaService.cs
@@ -1,6 +1,9 @@
-﻿namespace Clinica.API.Services;
+﻿using Clinica.Domain.DTOs.Auditoria;
+
+namespace Clinica.API.Services;
 
 public interface IAuditoriaService
 {
-    
+    Task<IEnumerable<AuditoriaResponseDto>> ObtenerTodosAsync();
+    Task<IEnumerable<AuditoriaResponseDto>> ObtenerPorUsuarioAsync(Guid usuarioId);
 }
