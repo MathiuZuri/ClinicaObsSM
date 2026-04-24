c3bb3b8c22929c65f49bd898639b8f9fb674cd12
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:03:55 -0500

    Infrastructure/ Configurations  - PacienteConfiguration

--- Clinica.Infrastructure/Data/Configurations/PacienteConfiguration.cs
+++ Clinica.Infrastructure/Data/Configurations/PacienteConfiguration.cs
@@ -1,6 +1,83 @@
-﻿namespace Clinica.Infrastructure.Data.Configurations;
+﻿using Clinica.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class PacienteConfiguration
+namespace Clinica.Infrastructure.Data.Configurations;
+
+public class PacienteConfiguration : IEntityTypeConfiguration<Paciente>
 {
-    
+    public void Configure(EntityTypeBuilder<Paciente> builder)
+    {
+        builder.ToTable("Pacientes");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.CodigoPaciente)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.HasIndex(x => x.CodigoPaciente)
+            .IsUnique();
+
+        builder.Property(x => x.DNI)
+            .IsRequired()
+            .HasMaxLength(8);
+
+        builder.HasIndex(x => x.DNI)
+            .IsUnique();
+
+        builder.Property(x => x.Nombres)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Apellidos)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Sexo)
+            .IsRequired()
+            .HasMaxLength(1);
+
+        builder.Property(x => x.Celular)
+            .HasMaxLength(15);
+
+        builder.Property(x => x.Correo)
+            .HasMaxLength(150);
+
+        builder.Property(x => x.Direccion)
+            .HasMaxLength(250);
+
+        builder.Property(x => x.Estado)
+            .HasConversion<string>()
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(x => x.FechaRegistro)
+            .IsRequired();
+
+        builder.HasOne(x => x.Usuario)
+            .WithMany()
+            .HasForeignKey(x => x.UsuarioId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.HistorialClinico)
+            .WithOne(x => x.Paciente)
+            .HasForeignKey<HistorialClinico>(x => x.PacienteId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(x => x.Citas)
+            .WithOne(x => x.Paciente)
+            .HasForeignKey(x => x.PacienteId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(x => x.Atenciones)
+            .WithOne(x => x.Paciente)
+            .HasForeignKey(x => x.PacienteId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(x => x.Pagos)
+            .WithOne(x => x.Paciente)
+            .HasForeignKey(x => x.PacienteId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
