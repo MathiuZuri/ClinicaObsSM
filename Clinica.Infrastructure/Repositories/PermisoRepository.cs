fa535fb05586425edaf122fd72d2325139388268
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:15:07 -0500

    Infrastructure/ Reposoties  - PermisoRepository

--- Clinica.Infrastructure/Repositories/PermisoRepository.cs
+++ Clinica.Infrastructure/Repositories/PermisoRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace Clinica.Infrastructure.Repositories;
+﻿using Clinica.Domain.Entities;
+using Clinica.Domain.Interfaces;
+using Clinica.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
-public class PermisoRepository
+namespace Clinica.Infrastructure.Repositories;
+
+public class PermisoRepository : GenericRepository<Permiso>, IPermisoRepository
 {
-    
+    public PermisoRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Permiso?> ObtenerPorCodigoAsync(string codigo)
+    {
+        return await Context.Permisos
+            .FirstOrDefaultAsync(x => x.Codigo == codigo);
+    }
 }
