ff015448073d26d3f68580e2411c172d22370269
kevinraf <paricahuak40@gmail.com>
2026-04-24 10:38:28 -0500

    DOMAIN/Entities - RolPermiso

--- Clinica.Domain/Entities/RolPermiso.cs
+++ Clinica.Domain/Entities/RolPermiso.cs
@@ -2,5 +2,13 @@
 
 public class RolPermiso
 {
-    
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public Guid RolId { get; set; }
+    public Rol Rol { get; set; } = null!;
+
+    public Guid PermisoId { get; set; }
+    public Permiso Permiso { get; set; } = null!;
+
+    public DateTime FechaAsignacion { get; set; } = DateTime.UtcNow;
 }
