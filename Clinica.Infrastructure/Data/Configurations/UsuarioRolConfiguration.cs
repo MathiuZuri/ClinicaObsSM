3f6bcb3f12b9667a3badd3d78a21fced383374cc
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:59:35 -0500

    Infrastructure/ Configurations  - UsuarioRolConfiguration

--- Clinica.Infrastructure/Data/Configurations/UsuarioRolConfiguration.cs
+++ Clinica.Infrastructure/Data/Configurations/UsuarioRolConfiguration.cs
@@ -1,6 +1,34 @@
-﻿namespace Clinica.Infrastructure.Data.Configurations;
+﻿using Clinica.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class UsuarioRolConfiguration
+namespace Clinica.Infrastructure.Data.Configurations;
+
+public class UsuarioRolConfiguration : IEntityTypeConfiguration<UsuarioRol>
 {
-    
+    public void Configure(EntityTypeBuilder<UsuarioRol> builder)
+    {
+        builder.ToTable("UsuarioRoles");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.FechaAsignacion)
+            .IsRequired();
+
+        builder.Property(x => x.Activo)
+            .IsRequired();
+
+        builder.HasIndex(x => new { x.UsuarioId, x.RolId })
+            .IsUnique();
+
+        builder.HasOne(x => x.Usuario)
+            .WithMany(x => x.UsuarioRoles)
+            .HasForeignKey(x => x.UsuarioId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.Rol)
+            .WithMany(x => x.UsuarioRoles)
+            .HasForeignKey(x => x.RolId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
