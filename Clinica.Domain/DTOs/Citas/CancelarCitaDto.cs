ffbb1a4c2ce8ad9896f7fbd8f63b5ecf85cb80ad
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:35:19 -0500

    DOMAIN/ DTOS - CancelarCitaDto

--- Clinica.Domain/DTOs/Citas/CancelarCitaDto.cs
+++ Clinica.Domain/DTOs/Citas/CancelarCitaDto.cs
@@ -1,6 +1,7 @@
-﻿namespace Clinica.Domain.DTOs;
+﻿namespace Clinica.Domain.DTOs.Citas;
 
 public class CancelarCitaDto
 {
-    
+    public string MotivoCancelacion { get; set; } = string.Empty;
+    public Guid? UsuarioRegistroId { get; set; }
 }
