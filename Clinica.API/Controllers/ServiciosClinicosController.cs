9701ce7dd4b7290c62f4404ce4a5000d1197cead
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:46:29 -0500

    API/  Services IMP -  ServiciosClinicosController

--- Clinica.API/Controllers/ServiciosClinicosController.cs
+++ Clinica.API/Controllers/ServiciosClinicosController.cs
@@ -1,6 +1,35 @@
-﻿namespace Clinica.API.Controllers;
+﻿using Clinica.API.Services;
+using Microsoft.AspNetCore.Mvc;
 
-public class ServiciosClinicosController
+namespace Clinica.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ServiciosClinicosController : ControllerBase
 {
-    
+    private readonly IServicioClinicoService _servicioService;
+
+    public ServiciosClinicosController(IServicioClinicoService servicioService)
+    {
+        _servicioService = servicioService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await _servicioService.ObtenerTodosAsync());
+    }
+
+    [HttpGet("activos")]
+    public async Task<IActionResult> GetActivos()
+    {
+        return Ok(await _servicioService.ObtenerActivosAsync());
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var servicio = await _servicioService.ObtenerPorIdAsync(id);
+        return servicio == null ? NotFound(new { mensaje = "Servicio clínico no encontrado." }) : Ok(servicio);
+    }
 }
