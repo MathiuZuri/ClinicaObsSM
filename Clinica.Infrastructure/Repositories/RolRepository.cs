8da7817ebcfb429df0fbc0157c82829d5754f2d7
kevinraf <paricahuak40@gmail.com>
2026-04-24 12:14:53 -0500

    Infrastructure/ Reposoties  - RolRepository

--- Clinica.Infrastructure/Repositories/RolRepository.cs
+++ Clinica.Infrastructure/Repositories/RolRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace Clinica.Infrastructure.Repositories;
+﻿using Clinica.Domain.Entities;
+using Clinica.Domain.Interfaces;
+using Clinica.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
-public class RolRepository
+namespace Clinica.Infrastructure.Repositories;
+
+public class RolRepository : GenericRepository<Rol>, IRolRepository
 {
-    
+    public RolRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Rol?> ObtenerPorNombreAsync(string nombre)
+    {
+        return await Context.Roles
+            .Include(x => x.RolPermisos)
+            .ThenInclude(x => x.Permiso)
+            .FirstOrDefaultAsync(x => x.Nombre == nombre);
+    }
 }
