c4fc7b038b985fbcfd22391575053bcba22f52bc
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:24:36 -0500

    DOMAIN/ DTOS - RespuestaInicioSesionDto

--- Clinica.Domain/DTOs/Auth/RespuestaInicioSesionDto.cs
+++ Clinica.Domain/DTOs/Auth/RespuestaInicioSesionDto.cs
@@ -2,5 +2,11 @@
 
 public class RespuestaInicioSesionDto
 {
-    
+    public Guid UsuarioId { get; set; }
+    public string CodigoUsuario { get; set; } = string.Empty;
+    public string NombreCompleto { get; set; } = string.Empty;
+    public string Correo { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+    public List<string> Roles { get; set; } = new();
+    public List<string> Permisos { get; set; } = new();
 }
