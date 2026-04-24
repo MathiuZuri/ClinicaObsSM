5fca6041b32ea5b226fdf4d765d737f355c894ed
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:27:47 -0500

    DOMAIN/ DTOS - CrearPacienteDto

--- Clinica.Domain/DTOs/Pacientes/CrearPacienteDto.cs
+++ Clinica.Domain/DTOs/Pacientes/CrearPacienteDto.cs
@@ -1,18 +1,15 @@
-﻿namespace Clinica.Domain.DTOs;
+﻿namespace Clinica.Domain.DTOs.Pacientes;
 
 public class CrearPacienteDto
 {
-    // Nota: Por ahora pedimos el UsuarioId, pero en el futuro esto 
-    // se sacará automáticamente del token de quien inició sesión.
-    public Guid UsuarioId { get; set; } 
     public string DNI { get; set; } = string.Empty;
-    public string NumeroHC { get; set; } = string.Empty;
     public string Nombres { get; set; } = string.Empty;
     public string Apellidos { get; set; } = string.Empty;
     public DateTime FechaNacimiento { get; set; }
-    public string Sexo { get; set; } = "F"; // Por defecto Femenino en obstetricia
-    
+    public string Sexo { get; set; } = string.Empty;
     public string? Celular { get; set; }
-    public string? CorreoSecundario { get; set; }
+    public string? Correo { get; set; }
     public string? Direccion { get; set; }
+
+    public Guid UsuarioId { get; set; }
 }
