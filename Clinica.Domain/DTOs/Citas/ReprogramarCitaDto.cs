6c3b9957fc7b395046eb73ca71ada497dc0bb3e9
kevinraf <paricahuak40@gmail.com>
2026-04-24 11:35:06 -0500

    DOMAIN/ DTOS - ReprogramarCitaDto

--- Clinica.Domain/DTOs/Citas/ReprogramarCitaDto.cs
+++ Clinica.Domain/DTOs/Citas/ReprogramarCitaDto.cs
@@ -1,6 +1,14 @@
-﻿namespace Clinica.Domain.DTOs;
+﻿namespace Clinica.Domain.DTOs.Citas;
 
 public class ReprogramarCitaDto
 {
-    
+    public Guid DoctorId { get; set; }
+    public Guid? HorarioDoctorId { get; set; }
+
+    public DateOnly NuevaFecha { get; set; }
+    public TimeOnly NuevaHoraInicio { get; set; }
+    public TimeOnly NuevaHoraFin { get; set; }
+
+    public string? MotivoReprogramacion { get; set; }
+    public Guid? UsuarioRegistroId { get; set; }
 }
