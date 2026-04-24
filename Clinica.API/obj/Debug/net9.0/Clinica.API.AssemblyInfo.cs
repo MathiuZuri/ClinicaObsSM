f46d3099b5e7e9110798008a6b5782f2d744e26b
kevinraf <paricahuak40@gmail.com>
2026-04-24 13:00:35 -0500

    API/  DB confuiguartion paswoord

--- Clinica.API/obj/Debug/net9.0/Clinica.API.AssemblyInfo.cs
+++ Clinica.API/obj/Debug/net9.0/Clinica.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clinica.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6df4106af362a1e92ffc84a9bf8bab5331192848")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678f05261a1e842bffd8c42b7d5a0f94473d6a6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clinica.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clinica.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
