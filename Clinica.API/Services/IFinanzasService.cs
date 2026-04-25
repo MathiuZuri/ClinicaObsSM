893ce2c16f1d2e31fd243316d2c79bfc3efac791
kevinraf <paricahuak40@gmail.com>
2026-04-25 16:06:35 -0500

    Domain / IFinanzasService

---
+++ Clinica.API/Services/IFinanzasService.cs
@@ -0,0 +1,17 @@
+﻿using Clinica.Domain.DTOs.Finanzas;
+
+namespace Clinica.API.Services;
+
+public interface IFinanzasService
+{
+    Task<ResumenDiarioFinanzasDto> ObtenerResumenDiarioAsync(DateOnly fecha);
+    Task<ResumenMensualFinanzasDto> ObtenerResumenMensualAsync(int anio, int mes);
+    Task<ResumenAnualFinanzasDto> ObtenerResumenAnualAsync(int anio);
+
+    Task<IEnumerable<PagoFinanzasDto>> ObtenerPagosPendientesAsync();
+    Task<IEnumerable<PagoFinanzasDto>> ObtenerPagosPagadosAsync();
+    Task<IEnumerable<PagoFinanzasDto>> ObtenerPagosParcialesAsync();
+
+    Task<PagoFinanzasDto?> ObtenerPagoPorCodigoAsync(string codigoPago);
+    Task<EstadoCuentaPacienteDto> ObtenerEstadoCuentaPacienteAsync(Guid pacienteId);
+}
