5863a9f9edf0b45f428470a77b29c4fb889315bf
kevinraf <paricahuak40@gmail.com>
2026-04-25 00:05:28 -0500

    API / Correciones EditarHorarioDoctorDto

--- Clinica.Domain/DTOs/Horarios/EditarHorarioDoctorDto.cs
+++ Clinica.Domain/DTOs/Horarios/EditarHorarioDoctorDto.cs
@@ -3,9 +3,14 @@
 public class EditarHorarioDoctorDto
 {
     public DayOfWeek DiaSemana { get; set; }
+
     public TimeOnly HoraInicio { get; set; }
+
     public TimeOnly HoraFin { get; set; }
+
     public DateOnly FechaInicioVigencia { get; set; }
+
     public DateOnly? FechaFinVigencia { get; set; }
+
     public bool Activo { get; set; } = true;
 }
