e82a8efec83ff5e7307c2b9129513318d75b0a9d
kevinraf <paricahuak40@gmail.com>
2026-04-25 00:05:15 -0500

    API / Correciones CrearHorarioDoctorDto

--- Clinica.Domain/DTOs/Horarios/CrearHorarioDoctorDto.cs
+++ Clinica.Domain/DTOs/Horarios/CrearHorarioDoctorDto.cs
@@ -1,11 +1,19 @@
-﻿namespace Clinica.Domain.DTOs.Horarios;
+﻿using Clinica.Domain.Validations;
+
+namespace Clinica.Domain.DTOs.Horarios;
 
 public class CrearHorarioDoctorDto
 {
+    [NotEmptyGuid(ErrorMessage = "El doctor es obligatorio.")]
     public Guid DoctorId { get; set; }
+
     public DayOfWeek DiaSemana { get; set; }
+
     public TimeOnly HoraInicio { get; set; }
+
     public TimeOnly HoraFin { get; set; }
+
     public DateOnly FechaInicioVigencia { get; set; }
+
     public DateOnly? FechaFinVigencia { get; set; }
 }
