733a48111cc478b4eee0a02cec18593cd56e0721
kevinraf <paricahuak40@gmail.com>
2026-04-25 00:07:02 -0500

    API / Correciones AsignarRolUsuarioDto

--- Clinica.Domain/DTOs/Usuarios/AsignarRolUsuarioDto.cs
+++ Clinica.Domain/DTOs/Usuarios/AsignarRolUsuarioDto.cs
@@ -1,7 +1,12 @@
-﻿namespace Clinica.Domain.DTOs.Usuarios;
+﻿using Clinica.Domain.Validations;
+
+namespace Clinica.Domain.DTOs.Usuarios;
 
 public class AsignarRolUsuarioDto
 {
+    [NotEmptyGuid(ErrorMessage = "El usuario es obligatorio.")]
     public Guid UsuarioId { get; set; }
+
+    [NotEmptyGuid(ErrorMessage = "El rol es obligatorio.")]
     public Guid RolId { get; set; }
 }
