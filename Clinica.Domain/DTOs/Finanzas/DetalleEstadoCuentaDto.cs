11b7d16f49a4f4359859c8ecae2eae77a0150f54
kevinraf <paricahuak40@gmail.com>
2026-04-25 15:51:12 -0500

    Domain / DetalleEstadoCuentaDto

--- Clinica.Domain/DTOs/Finanzas/DetalleEstadoCuentaDto.cs
+++ Clinica.Domain/DTOs/Finanzas/DetalleEstadoCuentaDto.cs
@@ -2,5 +2,16 @@
 
 public class DetalleEstadoCuentaDto
 {
-    
+    public Guid PagoId { get; set; }
+    public string CodigoPago { get; set; } = string.Empty;
+
+    public Guid? AtencionId { get; set; }
+    public string Servicio { get; set; } = string.Empty;
+
+    public decimal MontoTotal { get; set; }
+    public decimal MontoPagado { get; set; }
+    public decimal SaldoPendiente { get; set; }
+
+    public string EstadoPago { get; set; } = string.Empty;
+    public DateTime FechaPago { get; set; }
 }
