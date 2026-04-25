935e007668f7ec1b1c2a2c7fcd133ef4659a9569
kevinraf <paricahuak40@gmail.com>
2026-04-24 22:05:45 -0500

    API / UsuarioActualService

---
+++ Clinica.API/Services/Imp/UsuarioActualService.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Claims;
+
+namespace Clinica.API.Services.Imp;
+
+public class UsuarioActualService : IUsuarioActualService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public UsuarioActualService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public Guid ObtenerUsuarioId()
+    {
+        var usuarioId = ObtenerUsuarioIdOpcional();
+
+        if (usuarioId == null)
+            throw new UnauthorizedAccessException("No se pudo identificar al usuario autenticado.");
+
+        return usuarioId.Value;
+    }
+
+    public Guid? ObtenerUsuarioIdOpcional()
+    {
+        var claim = _httpContextAccessor.HttpContext?
+            .User
+            .FindFirst(ClaimTypes.NameIdentifier)?
+            .Value;
+
+        return Guid.TryParse(claim, out var usuarioId)
+            ? usuarioId
+            : null;
+    }
+}
