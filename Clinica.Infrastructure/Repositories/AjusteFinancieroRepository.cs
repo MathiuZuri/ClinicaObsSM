6b811f11eb2aec704861b17875b579a83c73fe33
kevinraf <paricahuak40@gmail.com>
2026-04-26 11:42:13 -0500

    Domain / AjusteFinancieroRepository

---
+++ Clinica.Infrastructure/Repositories/AjusteFinancieroRepository.cs
@@ -0,0 +1,48 @@
+﻿using Clinica.Domain.Entities;
+using Clinica.Domain.Interfaces;
+using Clinica.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clinica.Infrastructure.Repositories;
+
+public class AjusteFinancieroRepository : GenericRepository<AjusteFinanciero>, IAjusteFinancieroRepository
+{
+    public AjusteFinancieroRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<AjusteFinanciero>> ObtenerTodosConDetalleAsync()
+    {
+        return await Context.AjustesFinancieros
+            .Include(x => x.Pago)
+            .Include(x => x.Paciente)
+            .Include(x => x.Atencion)
+            .Include(x => x.UsuarioRegistro)
+            .OrderByDescending(x => x.FechaRegistro)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<AjusteFinanciero>> ObtenerPorAtencionAsync(Guid atencionId)
+    {
+        return await Context.AjustesFinancieros
+            .Include(x => x.Pago)
+            .Include(x => x.Paciente)
+            .Include(x => x.Atencion)
+            .Include(x => x.UsuarioRegistro)
+            .Where(x => x.AtencionId == atencionId)
+            .OrderByDescending(x => x.FechaRegistro)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<AjusteFinanciero>> ObtenerPorPagoAsync(Guid pagoId)
+    {
+        return await Context.AjustesFinancieros
+            .Include(x => x.Pago)
+            .Include(x => x.Paciente)
+            .Include(x => x.Atencion)
+            .Include(x => x.UsuarioRegistro)
+            .Where(x => x.PagoId == pagoId)
+            .OrderByDescending(x => x.FechaRegistro)
+            .ToListAsync();
+    }
+}
