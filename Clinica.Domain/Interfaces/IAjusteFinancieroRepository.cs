fbb0b0cf3eb4cee889cb3e496bd96541503ad98b
kevinraf <paricahuak40@gmail.com>
2026-04-26 11:41:23 -0500

    Domain / IAjusteFinancieroRepository

---
+++ Clinica.Domain/Interfaces/IAjusteFinancieroRepository.cs
@@ -0,0 +1,10 @@
+﻿using Clinica.Domain.Entities;
+
+namespace Clinica.Domain.Interfaces;
+
+public interface IAjusteFinancieroRepository : IGenericRepository<AjusteFinanciero>
+{
+    Task<IEnumerable<AjusteFinanciero>> ObtenerTodosConDetalleAsync();
+    Task<IEnumerable<AjusteFinanciero>> ObtenerPorAtencionAsync(Guid atencionId);
+    Task<IEnumerable<AjusteFinanciero>> ObtenerPorPagoAsync(Guid pagoId);
+}
