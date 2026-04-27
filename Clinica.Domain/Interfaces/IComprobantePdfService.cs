f889f52f115953a8d07ea9f6f44056d22e663221
kevinraf <paricahuak40@gmail.com>
2026-04-27 00:18:13 -0500

    DOCUMENT  IComprobantePdfService

--- Clinica.Domain/Interfaces/IComprobantePdfService.cs
+++ Clinica.Domain/Interfaces/IComprobantePdfService.cs
@@ -1,6 +1,14 @@
-﻿namespace Clinica.Domain.Interfaces;
+﻿using Clinica.Domain.DTOs.Comprobantes;
 
-public class IComprobantePdfService
+namespace Clinica.Domain.Interfaces;
+
+public interface IComprobantePdfService
 {
-    
+    Task<DocumentoGeneradoDto> GenerarBoletaPagoPdfAsync(Guid comprobanteId);
+    Task<DocumentoGeneradoDto> GenerarConstanciaCitaPdfAsync(Guid comprobanteId);
+    Task<DocumentoGeneradoDto> GenerarResumenAtencionPdfAsync(Guid comprobanteId);
+    Task<DocumentoGeneradoDto> GenerarEstadoCuentaPacientePdfAsync(Guid pacienteId);
+    Task<DocumentoGeneradoDto> GenerarHistoriaClinicaPdfAsync(Guid historialClinicoId);
+    Task<DocumentoGeneradoDto> GenerarReporteCajaDiarioPdfAsync(DateOnly fecha);
+    Task<DocumentoGeneradoDto> GenerarReporteFinancieroMensualPdfAsync(int anio, int mes);
 }
