5a61d8749fda92fe742db68b47f81cec41b66919
kevinraf <paricahuak40@gmail.com>
2026-04-27 00:16:24 -0500

    DOCUMENT  ComprobanteHistoriaClinicaPreviewDto2

--- Clinica.Domain/DTOs/Comprobantes/ComprobanteHistoriaClinicaPreviewDto.cs
+++ Clinica.Domain/DTOs/Comprobantes/ComprobanteHistoriaClinicaPreviewDto.cs
@@ -2,5 +2,18 @@
 
 public class ComprobanteHistoriaClinicaPreviewDto
 {
-    
+    public Guid HistorialClinicoId { get; set; }
+
+    public Guid PacienteId { get; set; }
+    public string Paciente { get; set; } = string.Empty;
+    public string DniPaciente { get; set; } = string.Empty;
+    public string CodigoPaciente { get; set; } = string.Empty;
+
+    public DateTime FechaRegistroPaciente { get; set; }
+
+    public int CantidadAtenciones { get; set; }
+    public int CantidadCitas { get; set; }
+
+    public List<ComprobanteAtencionPreviewDto> Atenciones { get; set; } = new();
+    public List<ComprobanteCitaPreviewDto> Citas { get; set; } = new();
 }
