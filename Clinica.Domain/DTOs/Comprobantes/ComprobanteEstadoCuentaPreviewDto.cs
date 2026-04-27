b473586c1fc8ff4f2934bfba5728988c186b738a
kevinraf <paricahuak40@gmail.com>
2026-04-27 00:15:56 -0500

    DOCUMENT  ComprobanteEstadoCuentaPreviewDto

--- Clinica.Domain/DTOs/Comprobantes/ComprobanteEstadoCuentaPreviewDto.cs
+++ Clinica.Domain/DTOs/Comprobantes/ComprobanteEstadoCuentaPreviewDto.cs
@@ -2,5 +2,16 @@
 
 public class ComprobanteEstadoCuentaPreviewDto
 {
-    
+    public Guid PacienteId { get; set; }
+
+    public string Paciente { get; set; } = string.Empty;
+    public string DniPaciente { get; set; } = string.Empty;
+
+    public decimal TotalFacturado { get; set; }
+    public decimal TotalPagado { get; set; }
+    public decimal TotalPendiente { get; set; }
+
+    public int CantidadPagos { get; set; }
+
+    public List<ComprobantePagoPreviewDto> Pagos { get; set; } = new();
 }
