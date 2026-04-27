aa0968d1fb7d4edc2df7ad265f263b7a3208d185
kevinraf <paricahuak40@gmail.com>
2026-04-27 00:14:58 -0500

    DOCUMENT  EmitirComprobanteAtencionDto

--- Clinica.Domain/DTOs/Comprobantes/EmitirComprobanteAtencionDto.cs
+++ Clinica.Domain/DTOs/Comprobantes/EmitirComprobanteAtencionDto.cs
@@ -1,6 +1,12 @@
-﻿namespace Clinica.Domain.DTOs.Comprobantes;
+﻿using Clinica.Domain.Enums;
+
+namespace Clinica.Domain.DTOs.Comprobantes;
 
 public class EmitirComprobanteAtencionDto
 {
-    
+    public Guid AtencionId { get; set; }
+
+    public TipoFormatoImpresion FormatoImpresion { get; set; } = TipoFormatoImpresion.A4;
+
+    public string? Observacion { get; set; }
 }
