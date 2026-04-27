3cc2efafe491e3eeee151e35be1ac5d44560ee1b
kevinraf <paricahuak40@gmail.com>
2026-04-27 00:22:17 -0500

    DOCUMENT  ComprobanteDetalleConfiguration

--- Clinica.Infrastructure/Data/Configurations/ComprobanteDetalleConfiguration.cs
+++ Clinica.Infrastructure/Data/Configurations/ComprobanteDetalleConfiguration.cs
@@ -1,6 +1,50 @@
-﻿namespace Clinica.Infrastructure.Data.Configurations;
+﻿using Clinica.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class ComprobanteDetalleConfiguration
+namespace Clinica.Infrastructure.Data.Configurations;
+
+public class ComprobanteDetalleConfiguration : IEntityTypeConfiguration<ComprobanteDetalle>
 {
-    
+    public void Configure(EntityTypeBuilder<ComprobanteDetalle> builder)
+    {
+        builder.ToTable("ComprobanteDetalles");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Descripcion)
+            .IsRequired()
+            .HasMaxLength(250);
+
+        builder.Property(x => x.Cantidad)
+            .HasPrecision(10, 2)
+            .IsRequired();
+
+        builder.Property(x => x.PrecioUnitario)
+            .HasPrecision(10, 2)
+            .IsRequired();
+
+        builder.Property(x => x.Subtotal)
+            .HasPrecision(10, 2)
+            .IsRequired();
+
+        builder.Property(x => x.PorcentajeImpuesto)
+            .HasPrecision(5, 2)
+            .IsRequired();
+
+        builder.Property(x => x.MontoImpuesto)
+            .HasPrecision(10, 2)
+            .IsRequired();
+
+        builder.Property(x => x.Total)
+            .HasPrecision(10, 2)
+            .IsRequired();
+
+        builder.HasIndex(x => x.ComprobanteId);
+
+        builder.HasOne(x => x.Comprobante)
+            .WithMany(x => x.Detalles)
+            .HasForeignKey(x => x.ComprobanteId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
