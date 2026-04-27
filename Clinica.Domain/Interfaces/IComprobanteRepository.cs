89acd8061c51b58c30b195657e54ed8fa87f5826
kevinraf <paricahuak40@gmail.com>
2026-04-27 00:17:44 -0500

    DOCUMENT  IComprobanteRepository

--- Clinica.Domain/Interfaces/IComprobanteRepository.cs
+++ Clinica.Domain/Interfaces/IComprobanteRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace Clinica.Domain.Interfaces;
+﻿using Clinica.Domain.Entities;
+using Clinica.Domain.Enums;
 
-public class IComprobanteRepository
+namespace Clinica.Domain.Interfaces;
+
+public interface IComprobanteRepository : IGenericRepository<Comprobante>
 {
-    
+    Task<Comprobante?> ObtenerPorIdConDetalleAsync(Guid id);
+
+    Task<IEnumerable<Comprobante>> ObtenerPorPacienteAsync(Guid pacienteId);
+    Task<IEnumerable<Comprobante>> ObtenerPorPagoAsync(Guid pagoId);
+    Task<IEnumerable<Comprobante>> ObtenerPorCitaAsync(Guid citaId);
+    Task<IEnumerable<Comprobante>> ObtenerPorAtencionAsync(Guid atencionId);
+    Task<IEnumerable<Comprobante>> ObtenerPorHistorialClinicoAsync(Guid historialClinicoId);
+
+    Task<Comprobante?> ObtenerUltimoPorSerieAsync(string serie, TipoComprobante tipoComprobante);
+
+    Task<bool> ExisteComprobanteActivoPorPagoAsync(Guid pagoId, TipoComprobante tipoComprobante);
+    Task<bool> ExisteComprobanteActivoPorCitaAsync(Guid citaId, TipoComprobante tipoComprobante);
+    Task<bool> ExisteComprobanteActivoPorAtencionAsync(Guid atencionId, TipoComprobante tipoComprobante);
+    Task<bool> ExisteComprobanteActivoPorHistorialClinicoAsync(Guid historialClinicoId, TipoComprobante tipoComprobante);
 }
