690d060d90dc4b14c50f4730fc5de8fc8aa16cab
kevinraf <paricahuak40@gmail.com>
2026-04-27 00:18:00 -0500

    DOCUMENT  IComprobanteService

--- Clinica.Domain/Interfaces/IComprobanteService.cs
+++ Clinica.Domain/Interfaces/IComprobanteService.cs
@@ -1,6 +1,28 @@
-﻿namespace Clinica.Domain.Interfaces;
+﻿using Clinica.Domain.DTOs.Comprobantes;
 
-public class IComprobanteService
+namespace Clinica.Domain.Interfaces;
+
+public interface IComprobanteService
 {
-    
+    Task<ComprobantePagoPreviewDto> PreviewBoletaPagoAsync(Guid pagoId, decimal tasaImpuesto = 0.18m);
+    Task<Guid> EmitirBoletaPagoAsync(EmitirComprobantePagoDto dto);
+
+    Task<ComprobanteCitaPreviewDto> PreviewConstanciaCitaAsync(Guid citaId);
+    Task<Guid> EmitirConstanciaCitaAsync(EmitirComprobanteCitaDto dto);
+
+    Task<ComprobanteAtencionPreviewDto> PreviewResumenAtencionAsync(Guid atencionId);
+    Task<Guid> EmitirResumenAtencionAsync(EmitirComprobanteAtencionDto dto);
+
+    Task<ComprobanteEstadoCuentaPreviewDto> PreviewEstadoCuentaPacienteAsync(Guid pacienteId);
+
+    Task<ComprobanteHistoriaClinicaPreviewDto> PreviewHistoriaClinicaAsync(Guid historialClinicoId);
+    Task<Guid> EmitirHistoriaClinicaAsync(Guid historialClinicoId);
+
+    Task<ComprobanteDto> ObtenerComprobanteAsync(Guid comprobanteId);
+
+    Task<IEnumerable<ComprobanteDto>> ObtenerComprobantesPorPacienteAsync(Guid pacienteId);
+    Task<IEnumerable<ComprobanteDto>> ObtenerComprobantesPorPagoAsync(Guid pagoId);
+    Task<IEnumerable<ComprobanteDto>> ObtenerComprobantesPorAtencionAsync(Guid atencionId);
+
+    Task AnularComprobanteAsync(Guid comprobanteId, string motivo);
 }
