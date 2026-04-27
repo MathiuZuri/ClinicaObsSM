433864c032fcfc0bddc6f23ccdc38ec0080106cc
kevinraf <paricahuak40@gmail.com>
2026-04-27 00:16:59 -0500

    DOCUMENT  ComprobanteHistoriaClinicaPreviewDto2

--- Clinica.Domain/DTOs/Comprobantes/DocumentoGeneradoDto.cs
+++ Clinica.Domain/DTOs/Comprobantes/DocumentoGeneradoDto.cs
@@ -2,5 +2,7 @@
 
 public class DocumentoGeneradoDto
 {
-    
+    public string NombreArchivo { get; set; } = string.Empty;
+    public string TipoContenido { get; set; } = "application/pdf";
+    public byte[] Contenido { get; set; } = Array.Empty<byte>();
 }
