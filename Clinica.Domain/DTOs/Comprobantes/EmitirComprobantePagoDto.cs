4552d56c8122d1aa4a158c098d2ad38b2fc0649d
kevinraf <paricahuak40@gmail.com>
2026-04-27 00:14:34 -0500

    DOCUMENT  EmitirComprobantePagoDto

--- Clinica.Domain/DTOs/Comprobantes/EmitirComprobantePagoDto.cs
+++ Clinica.Domain/DTOs/Comprobantes/EmitirComprobantePagoDto.cs
@@ -1,6 +1,14 @@
-﻿namespace Clinica.Domain.DTOs.Comprobantes;
+﻿using Clinica.Domain.Enums;
+
+namespace Clinica.Domain.DTOs.Comprobantes;
 
 public class EmitirComprobantePagoDto
 {
-    
+    public Guid PagoId { get; set; }
+
+    public decimal TasaImpuesto { get; set; } = 0.18m;
+
+    public TipoFormatoImpresion FormatoImpresion { get; set; } = TipoFormatoImpresion.A4;
+
+    public string? Observacion { get; set; }
 }
