d955c4eadd4a654aeb6e07c87a09806fe15b60cf
kevinraf <paricahuak40@gmail.com>
2026-04-29 09:44:33 -0500

    Comprovantes | IComprobanteRepository

--- Clinica.Domain/Interfaces/IComprobanteRepository.cs
+++ Clinica.Domain/Interfaces/IComprobanteRepository.cs
@@ -5,10 +5,16 @@ namespace Clinica.Domain.Interfaces;
 public interface IComprobanteRepository : IGenericRepository<Comprobante>
 {
     Task<IEnumerable<Comprobante>> ObtenerTodosConDetalleAsync();
+
     Task<Comprobante?> ObtenerPorIdConDetalleAsync(Guid id);
+
     Task<IEnumerable<Comprobante>> ObtenerPorPacienteAsync(Guid pacienteId);
+
     Task<IEnumerable<Comprobante>> ObtenerPorPagoAsync(Guid pagoId);
+
     Task<IEnumerable<Comprobante>> ObtenerPorAtencionAsync(Guid atencionId);
+
     Task<Comprobante?> ObtenerPorSerieNumeroAsync(string serie, int numero);
+
     Task<int> ObtenerUltimoNumeroPorSerieAsync(string serie);
 }
